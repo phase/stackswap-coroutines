94b67ab81462055a3105236fb40c5ded9167771d
jadon fowler <j@jadon.io>
2025-01-09 20:17:47 -0800

    fix swapping stacks & cleanup in release mode
    cleanup is still a lil broken

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 Coroutines implemented by swapping between stacks.
 
-CRASHES with `--release`!
-
 A reimplementation of some parts of [usched](https://github.com/nikitadanilov/usched) in Rust.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#![feature(strict_provenance)]
-
 use setjmp::*;
 use std::cell::RefCell;
 use std::ffi::c_void;
@@ -43,6 +41,10 @@ impl Dispatcher {
 
         // while we have a stack to use
         while let Some(stack) = self.next() {
+            if stack.is_complete {
+                self.stacks.remove(self.current_stack);
+                continue;
+            }
             // set this stack as the current
             // CURRENT.with(|current| {
             //     if let Ok(mut current) = current.try_borrow_mut() {
@@ -53,14 +55,8 @@ impl Dispatcher {
             // });
 
             // continue/launch the stack
-            let id = stack.id;
             if let Some(_) = (*stack).bottom {
                 stack.cont();
-
-                self.stacks.retain(|x| x.id != id);
-                if self.current_stack > 0 {
-                    self.current_stack -= 1;
-                }
             } else {
                 stack.launch();
             }
@@ -108,6 +104,7 @@ pub struct Stack {
     stack_buffer: Option<StackBuffer>,
     continue_jmpbuf: *mut jmp_buf,
     program: Option<Box<dyn FnOnce(&mut Stack)>>,
+    is_complete: bool,
 }
 
 impl Stack {
@@ -120,6 +117,7 @@ impl Stack {
             stack_buffer: None,
             continue_jmpbuf: std::ptr::null_mut(),
             program: Some(program),
+            is_complete: false,
         }
     }
 
@@ -145,6 +143,7 @@ impl Stack {
             self.bottom.unwrap().addr() > self.top.addr(),
             "used must be greater than 0"
         );
+
         if let Some(stack_buffer) = self.stack_buffer.take() {
             if stack_buffer.len < used {
                 unsafe {
@@ -154,28 +153,24 @@ impl Stack {
                 self.stack_buffer = Some(stack_buffer);
             }
         }
-        if let None = &self.stack_buffer {
-            // allocate a new stack buffer
-            // eprintln!(
-            //     "requesting stack buffer of size {} for stack {}",
-            //     used, self.id
-            // );
+
+        if self.stack_buffer.is_none() {
             self.stack_buffer = Some(unsafe { (*self.dispatcher).allocator.alloc(used) });
         }
 
         unsafe {
-            std::ptr::copy_nonoverlapping(self.top, self.stack_buffer.as_ref().unwrap().loc, used);
+            let buffer = self.stack_buffer.as_ref().unwrap();
+            std::ptr::copy_nonoverlapping(self.top, buffer.loc, used);
         }
     }
 
     #[inline(never)]
     pub fn cont(&mut self) {
-        // maybe the stack has two things here?
-        // 1. a value to stuff once the stack is in
-        // 2. a location to stuff the value
-        // The location is only valid _after_ the stack is in.
+        if self.is_complete {
+            eprintln!("Warning: Attempting to continue completed stack {}", self.id);
+            return;
+        }
         self.stack_in();
-        // TODO: insert continuation value on the stack here
         unsafe {
             longjmp(self.continue_jmpbuf, 1);
         }
@@ -183,11 +178,14 @@ impl Stack {
 
     #[inline(never)]
     pub fn launch(&mut self) {
-        // pad the stack to prevent stack_in() from smashing the frame
-        let mut padding = [0u8; 512];
+        // Increase padding to be safe
+        let mut padding = [0u8; 1024];
         self.bottom = Some(padding.as_mut_ptr() as *mut c_void);
         if let Some(program) = self.program.take() {
             program(self);
+            // Don't save the stack state when we're done
+            self.bottom = None;
+            self.is_complete = true;
         }
     }
 
@@ -197,43 +195,101 @@ impl Stack {
             let mut jmpbuf: MaybeUninit<jmp_buf> = MaybeUninit::uninit();
             if setjmp(jmpbuf.as_mut_ptr()) == 0 {
                 self.continue_jmpbuf = jmpbuf.as_mut_ptr();
+                // Increase the offset to avoid potential stack corruption
                 self.top = self
                     .continue_jmpbuf
-                    .with_addr(self.continue_jmpbuf.addr() - 32)
+                    .with_addr(self.continue_jmpbuf.addr() - 64)
                     as *mut c_void;
+                // Save stack state
                 self.stack_out();
+
+                // Jump back to dispatcher
                 longjmp((*self.dispatcher).jmpbuf, 1);
+            }
+            // We've returned from a continuation, no need to restore stack
+            // as it was already restored in cont()
+        }
+    }
+}
+
+impl Drop for Stack {
+    fn drop(&mut self) {
+        if let Some(buffer) = self.stack_buffer.take() {
+            unsafe {
+                (*self.dispatcher).allocator.free(buffer);
             }
         }
     }
 }
 
-struct HeapAllocator;
+struct HeapAllocator {
+    #[allow(dead_code)]
+    allocations: std::sync::atomic::AtomicUsize,
+}
+
+impl HeapAllocator {
+    fn new() -> Self {
+        Self {
+            allocations: std::sync::atomic::AtomicUsize::new(0)
+        }
+    }
+}
 
 impl StackBufferAllocator for HeapAllocator {
     fn alloc(&self, len: usize) -> StackBuffer {
-        let alloc = vec![7u8; len].into_boxed_slice();
+        let len = (len + 15) & !15;
+        let layout = std::alloc::Layout::from_size_align(len, 16)
+            .expect("Failed to create layout");
+        let ptr = unsafe { std::alloc::alloc_zeroed(layout) };
+        if ptr.is_null() {
+            std::alloc::handle_alloc_error(layout);
+        }
+
+        let count = self.allocations.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+        eprintln!("Allocating buffer {} at {:p}", count + 1, ptr);
+
         StackBuffer {
-            loc: Box::leak(alloc).as_ptr() as *mut c_void,
+            loc: ptr as *mut c_void,
             len,
         }
     }
 
     fn free(&self, stack_buffer: StackBuffer) {
+        let current = self.allocations.fetch_sub(1, std::sync::atomic::Ordering::SeqCst);
+        if current == 0 {
+            eprintln!("Warning: Attempting to free buffer at {:p} when allocation count is 0",
+                     stack_buffer.loc);
+            self.allocations.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+            return;
+        }
+
+        eprintln!("Freeing buffer {} at {:p}", current, stack_buffer.loc);
+
+        let len = (stack_buffer.len + 15) & !15;
         unsafe {
-            drop(Box::from_raw(stack_buffer.loc));
+            let layout = std::alloc::Layout::from_size_align(len, 16)
+                .expect("Failed to create layout");
+            std::alloc::dealloc(stack_buffer.loc as *mut u8, layout);
         }
     }
 }
 
 fn test_coroutine(stack: &mut Stack, counter: *mut u64) {
+    let mut local = 0u64;
     unsafe {
         loop {
-            if *counter >= 6 {
+            if *counter >= 1_000_000 {
+                *counter += 1;
+                if *counter % 1_000 == 0 {
+                    eprintln!("DONEstack {} | local:{} counter:{}", stack.id, local, *counter);
+                }
                 return;
+            } else if *counter % 10_000 == 0 {
+                eprintln!("stack {} | local:{} counter:{}", stack.id, local, *counter);
             }
+
+            local += *counter / 2;
             *counter += 1;
-            eprintln!("{}: {}", stack.id, *counter);
             stack.block();
         }
     }
@@ -241,11 +297,11 @@ fn test_coroutine(stack: &mut Stack, counter: *mut u64) {
 
 fn main() {
     println!("Launching Dispatcher.");
-    let mut dispatcher = Dispatcher::new(Box::new(HeapAllocator));
+    let mut dispatcher = Dispatcher::new(Box::new(HeapAllocator::new()));
     let d = &mut dispatcher as *mut Dispatcher;
     let mut counter = Box::new(0u64);
     let counter_ptr = counter.as_mut() as *mut u64;
-    for i in 0..5 {
+    for i in 0..4 {
         let stack = Stack::new(
             i,
             d,
